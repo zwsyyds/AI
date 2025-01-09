4eecaf8a91ed7f9b5b772f05b3f3f0083cc8d2e0
zwsyyds <111140138+zwsyyds@users.noreply.github.com>
2025-01-09 16:46:38 +0800

    Update vue.config.js

--- lendada-frontend/vue.config.js
+++ lendada-frontend/vue.config.js
@@ -2,3 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+//eslint导致build 项目错误
+module.exports = {
+  lintOnSave: process.env.NODE_ENV !== 'production'
+}
