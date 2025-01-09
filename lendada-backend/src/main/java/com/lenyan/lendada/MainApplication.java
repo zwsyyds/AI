7e9b941b8fb3f146853285dec55aa2754631b4d7
zwsyyds <111140138+zwsyyds@users.noreply.github.com>
2025-01-09 21:27:44 +0800

    Update MainApplication.java

--- lendada-backend/src/main/java/com/lenyan/lendada/MainApplication.java
+++ lendada-backend/src/main/java/com/lenyan/lendada/MainApplication.java
@@ -12,7 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author <a href="https://github.com/lenyanjgk">程序员冷颜</a>
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan("com.lenyan.lendada.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
